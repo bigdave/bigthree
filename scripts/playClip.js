e3683478bf10cd78beaf46a31e77ba3f3f668e97
David Smith <bigdave.smith@gmail.com>
2015-10-28 07:58:22 -0700

    Cleanup to playClip

--- scripts/playClip.js
+++ scripts/playClip.js
@@ -2,13 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
 	document.addEventListener('click', function(e) {
 		if (e.target.nodeName === 'A' && e.target.getAttribute('role') === 'clip') {
 			e.preventDefault();
-			console.log("HERE");
 
-			readableTime = e.target.innerHTML.split(':');
+			var readableTime = e.target.innerHTML.split(':');
 			var seconds = parseInt(readableTime.pop());
 			var minutes = parseInt(readableTime.pop());
 			var hours = parseInt(readableTime.pop()) || 0;
-
 			var index = seconds + (60 * minutes) + (3600 * hours);
 
 			var audioClip = document.querySelector('audio');
