21e26b6932d6081c0ba5b8c77f4a325c5f7ec488
David Smith <bigdave.smith@gmail.com>
2015-10-27 22:49:24 -0700

    Improved clip link display

--- scripts/playClip.js
+++ scripts/playClip.js
@@ -1,10 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
 	document.addEventListener('click', function(e) {
-		if (e.target.nodeName === 'A' && e.target.dataset.index) {
+		if (e.target.nodeName === 'A' && e.target.getAttribute('role') === 'clip') {
 			e.preventDefault();
+			console.log("HERE");
+
+			readableTime = e.target.innerHTML.split(':');
+			var seconds = parseInt(readableTime.pop());
+			var minutes = parseInt(readableTime.pop());
+			var hours = parseInt(readableTime.pop()) || 0;
+
+			var index = seconds + (60 * minutes) + (3600 * hours);
 
 			var audioClip = document.querySelector('audio');
-			audioClip.currentTime = parseInt(e.target.dataset.index);
+			audioClip.currentTime = index;
 			audioClip.play();
 		}
 	});
